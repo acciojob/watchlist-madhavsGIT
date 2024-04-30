f7581d16c1a7f1f6a752f703ac97ca3900039791
madhavsGIT <kmadhav455@gamil.com>
2024-05-01 02:05:00 +0530

    firstcommit

--- src/main/java/com/driver/MovieRepository.java
+++ src/main/java/com/driver/MovieRepository.java
@@ -19,28 +19,58 @@ public class MovieRepository {
 
     public void saveMovie(Movie movie){
         // your code here
+        String key = movie.getName();
+        movieMap.put(key, movie);
     }
 
     public void saveDirector(Director director){
         // your code here
+        String key = director.getName();
+        directorMap.put(key, director);
     }
 
     public void saveMovieDirectorPair(String movie, String director){
         if(movieMap.containsKey(movie) && directorMap.containsKey(director)){
             // your code here
+            if (directorMovieMapping.containsKey(director)) {
+                List<String> movieList = directorMovieMapping.get(director);
+                if (!movieList.contains(movie)) {
+                    movieList.add(movie);
+                    directorMovieMapping.put(director, movieList);
+                }
+            } else {
+                List<String> newMovieList = new ArrayList<>();
+                newMovieList.add(movie);
+                directorMovieMapping.put(director, newMovieList);
+            }
+
         }
     }
 
     public Movie findMovie(String movie){
         // your code here
+        if(movieMap.containsKey(movie)){
+            return movieMap.get(movie);
+        }
+        return null;
     }
 
     public Director findDirector(String director){
         // your code here
+        if(directorMap.containsKey(director)){
+            return directorMap.get(director);
+        }
+        return null;
     }
 
     public List<String> findMoviesFromDirector(String director){
         // your code here
+
+        if(directorMovieMapping.containsKey(director)){
+            return directorMovieMapping.get(director);
+        }
+        return null;
+
     }
 
     public List<String> findAllMovies(){
@@ -49,9 +79,14 @@ public class MovieRepository {
 
     public void deleteDirector(String director){
         // your code here
+        if(directorMap.containsKey(director)){
+            directorMap.remove(director);
+            //directorMovieMapping.remove(director);
+        }
     }
 
     public void deleteAllDirector(){
         // your code here
+        directorMap.clear();
     }
 }
